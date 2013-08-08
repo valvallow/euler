5c63117d10743f19d317d12986cf89c923e38ef9
valvallow <valvalloooooooooow@gmail.com>
2013-08-09 00:14:47 +0900

    9 dooooone

---
+++ 0009.scm
@@ -0,0 +1,19 @@
+(use gauche.generator)
+(use srfi-42)
+
+;; https://github.com/shirok/Gauche/blob/master/test/srfi.scm
+(define (pythagoras n)
+  (list-ec
+   (:let sqr-n (* n n))
+   (:range a 1 (+ n 1))
+   (:let sqr-a (* a a))
+   (:range b a (+ n 1))
+   (:let sqr-c (+ sqr-a (* b b)))
+   (if (<= sqr-c sqr-n))
+   (:range c b (+ n 1))
+   (if (= (* c c) sqr-c))
+   (list a b c)))
+
+(dolist (ls (pythagoras 500))
+  (when (= 1000 (apply + ls))
+    (print (apply * ls))))
