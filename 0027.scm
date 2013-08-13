da79571b2a6d2dcd820b4f94803a2d88ff48f213
valvallow <valvalloooooooooow@gmail.com>
2013-08-13 19:37:35 +0900

    refactoring

--- 0027.scm
+++ 0027.scm
@@ -5,24 +5,33 @@
 ;; http://tsumuji.cocolog-nifty.com/tsumuji/2009/02/problem-27.html
 ;; http://tsumuji.cocolog-nifty.com/tsumuji/2010/06/project-euler-p.html
 
-(let ((a-max 0)
-      (b-max 0)
-      (n-max 0))
-  (dolist (b (take-while (cut < <> 1000) *primes*))
-    (let/cc break
-      (let1 a (- (quotient (- b) 40) 40)
-        (let rec ((a a))
-          (unless (< a 1000)
-            (break))
-          (if (even? a)
-              (rec (+ a 1))
-              (dolist (n (lrange 0))
-                (let1 v (+ (* n n)(* a n) b)
-                  (unless (and (positive? v)
-                               (bpsw-prime? v))
-                    (when (< n-max (- n 1))
-                      (set! n-max (- n 1))
-                      (set! a-max a)
-                      (set! b-max b))
-                    (rec (+ a 1))))))))))
-  (print (* a-max b-max)))
+(define (make-b-candidates limit)
+  (take-while (pa$ > limit) *primes*))
+
+(define (make-lazy-a-candidates limit a-start)
+  (lfilter odd?
+           (ltake-while (pa$ > limit)
+                        (lrange a-start))))
+
+(let* ((b-list (make-b-candidates 1000))
+       (a-lists (map (^b (make-lazy-a-candidates
+                          1000
+                          (- (quotient (- b) 40) 40)))
+                     b-list)))
+  (let ((a-max 0)
+        (b-max 0)
+        (n-max 0))
+    (for-each (^(b als)
+                (dolist (a als)
+                  (let/cc break
+                    (dolist (n (lrange 0))
+                      (let1 v (+ (* n n)(* a n) b)
+                        (unless (and (positive? v)
+                                     (bpsw-prime? v))
+                          (when (< n-max (- n 1))
+                            (set! n-max (- n 1))
+                            (set! a-max a)
+                            (set! b-max b))
+                          (break)))))))
+              b-list a-lists)
+    (print (* a-max b-max))))
