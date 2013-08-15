d511682c102472f6935bd239537907e2adb6eb21
valvallow <valvalloooooooooow@gmail.com>
2013-08-15 23:09:45 +0900

    refactoring

--- 0032.scm
+++ 0032.scm
@@ -4,17 +4,26 @@
        (string->list (number->string num))))
 
 
+(define (multiplier-exists? product multiplicand)
+  (zero? (mod product multiplicand)))
+
+(define (concat-numbers . nums)
+  (string->number (apply string-append (map number->string nums))))
+
+(define (pandigital-product? n)
+  (equal? (sort (number->list n))
+          (iota 9 1)))
+
 (define (pandigital-products)
   (delete-duplicates
    (append-map
     (^(product)
       (filter-map
        (^i (and-let*
-               (((zero? (mod product i)))
-                (nums (list product i (div product i)))
-                (numstr (apply string-append (map number->string nums)))
-                ((equal? (sort (number->list (string->number numstr)))
-                         (iota 9 1))))
+               (((multiplier-exists? product i))
+                (num (apply concat-numbers
+                            (list product i (div product i))))
+                ((pandigital-product? num)))
              product))
        (lrange 2 (sqrt product))))
     (lrange 1000 10000))))
